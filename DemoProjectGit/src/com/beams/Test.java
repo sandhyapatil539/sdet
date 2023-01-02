da6bfd169d12245d5cc5c190236c0ed995432134
sandhyapatil539 <121662149+sandhyapatil539@users.noreply.github.com>
2023-01-02 10:15:20 +0530

    First Commit

---
+++ DemoProjectGit/src/com/beams/Test.java
@@ -0,0 +1,24 @@
+package com.beams;
+
+public class Test {
+
+	
+		public  void test() {
+			 System.out.println("Welcome to selenium classes");
+
+
+		}
+		public static void main(String[] args) {
+		Test s=new Test();
+		s.test();
+		s.test();	
+		s.test();
+		}
+		
+
+
+
+
+	}
+
+
