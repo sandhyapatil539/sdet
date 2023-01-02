6e8518a1c616f41981780108f50a43c18227194f
Dev2 <Dev2@gmail.com>
2023-01-02 15:26:55 +0530

    Test

---
+++ DemoProjectGit/src/com/beams/t/Test2.java
@@ -0,0 +1,24 @@
+package com.beams.t;
+
+
+
+public class Test2 {
+
+		public  void test() {
+			 System.out.println("Welcome to selenium classes...");
+
+
+		}
+		public static void main(String[] args) {
+		Test2 s=new Test2();
+		s.test();
+		s.test();	
+		s.test();
+		}
+		
+
+
+
+
+	}
+
